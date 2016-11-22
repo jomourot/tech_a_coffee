1e5b914a67615ffc04bcd2e26cc46d57e450f637
Baptiste Saint-Aubin <baptiste.saintaubin@gmail.com>
2016-11-22 16:02:58 +0100

    search form for skils in Home and index des users

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,2 +1,24 @@
 class SkillsController < ApplicationController
+
+
+  def index
+    @skill = params[:name]
+    required_skills =  Skill.where(name: params[:name])
+    if required_skills.any?
+      @skills = required_skills
+      @found = true
+    else
+      @skills = Skill.all
+    end
+  end
+
 end
+
+
+
+
+
+
+
+
+
