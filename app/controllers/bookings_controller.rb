449c7814fb477f320210d7384f00eb3e8834bbf6
Ghislain Chavanne <gh.chavanne@gmail.com>
2016-11-22 18:58:36 +0100

    card user with booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,17 @@
 class BookingsController < ApplicationController
+  def create
+    @user = User.find(params[:user_id])
+    @booking = Booking.new(booking_params)
+    @booking.user = @booking
+    if @booking.save
+      redirect_to new_skill_user_booking_path(@user)
+    else
+      render "user/show"
+    end
+  end
+
+  private
+  def booking_params
+    params.require(:booking).permit(:starts_at, :duration, :accepted, :skill_id, :user_id)
+  end
 end
