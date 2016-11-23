32e102350f395a0db0458b2766d303fdf27a9528
Joseph Mourot <mourot.joseph@gmail.com>
2016-11-23 15:37:53 +0100

    finished integrating the map

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,16 +7,16 @@ jo.save!
 a = Skill.new(name: "seo", description: "j'ai travaillé dendant deux ans dans une agence Seo", meeting_point: "16 villa gaudelet", price:1, user_id: bapt.id)
 a.save!
 
-d = Skill.new(name: "sea", description: "j'ai travaillé dendant deux ans dans une agence sea", meeting_point: "16 villa gaudelet", price:1, user_id: jo.id)
+d = Skill.new(name: "sea", description: "j'ai travaillé dendant deux ans dans une agence sea", meeting_point: "41 rue des Francs-Bourgeois", price:1, user_id: jo.id)
 d.save!
 
-b = Skill.new(name: "sea", description: "Je connais très bien Google adwords", meeting_point: "16 villa gaudelet", price:1, user_id:jo.id)
+b = Skill.new(name: "sea", description: "Je connais très bien Google adwords", meeting_point: "17 rue des Fossés Saint Marcel", price:1, user_id:jo.id)
 b.save!
 
-e = Skill.new(name: "Ruby", description: "j'ai fait le wagon ", meeting_point: "16 villa gaudelet", price:1, user_id: jo.id)
+e = Skill.new(name: "Ruby", description: "j'ai fait le wagon ", meeting_point: "45 rue Obrekampf", price:1, user_id: jo.id)
 e.save!
 
-s = Skill.new(name: "Marketing digital", description: "j'ai travaillé en agence", meeting_point: "16 villa gaudelet", price:1, user_id: bapt.id)
+s = Skill.new(name: "Marketing digital", description: "j'ai travaillé en agence", meeting_point: "45 rue de Courcelle", price:1, user_id: bapt.id)
 s.save!
 
 q = Skill.new(name: "sea", description: "j'ai travaillé dendant deux ans dans une agence Seo", meeting_point: "16 villa gaudelet", price:1, user_id: bapt.id)
